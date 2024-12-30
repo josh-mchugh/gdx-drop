c72259288c9a400e2725d7dce05b2627fd3e5f3c
Josh McHugh <josh.mchugh@mailbox.org>
2024-12-29 21:19:31 -0500

    Removed Java Main class and added Scala Main class

---
+++ core/src/main/scala/net/sailware/drop/Main.scala
@@ -0,0 +1,12 @@
+package net.sailware.drop
+
+import com.badlogic.gdx.ApplicationListener
+
+class Main extends ApplicationListener:
+
+  override def create(): Unit = {}
+  override def resize(width: Int, height: Int): Unit = {}
+  override def render(): Unit = {}
+  override def pause(): Unit = {}
+  override def resume(): Unit = {}
+  override def dispose(): Unit = {}
