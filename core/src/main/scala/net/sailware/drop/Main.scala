6ede97e18b0cda1a1216e62d242083eccf9f5298
Josh McHugh <josh.mchugh@mailbox.org>
2024-12-31 22:21:49 -0500

    Extended the game to use Game and Screen implementations

--- core/src/main/scala/net/sailware/drop/Main.scala
+++ core/src/main/scala/net/sailware/drop/Main.scala
@@ -1,12 +1,14 @@
 package net.sailware.drop
 
-import com.badlogic.gdx.ApplicationListener
+import com.badlogic.gdx.Game
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
+import com.badlogic.gdx.Screen
 import com.badlogic.gdx.audio.Sound
 import com.badlogic.gdx.audio.Music
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.MathUtils
@@ -18,60 +20,94 @@ import com.badlogic.gdx.utils.viewport.FitViewport
 
 import scala.jdk.CollectionConverters._
 
-class Main extends ApplicationListener:
+class Main extends Game:
 
-  var backgroundTexture: Texture = null
-  var bucketSprite: Sprite = null
-  var dropTexture: Texture = null
-  var dropSound: Sound = null
-  var music: Music = null
-
-  var spriteBatch: SpriteBatch = null
+  var batch: SpriteBatch = null
+  var font: BitmapFont = null
   var viewport: FitViewport = null
 
-  var touchPos: Vector2 = null
+  override def create(): Unit =
+    batch = SpriteBatch()
+    font = BitmapFont()
+    viewport = new FitViewport(8, 5)
 
-  var dropSprites: Array[Sprite] = null
+    font.setUseIntegerPositions(false)
+    font.getData().setScale(viewport.getWorldHeight() / Gdx.graphics.getHeight())
+
+    this.setScreen(new MainMenuScreen(this))
+
+  override def render(): Unit =
+    super.render()
+
+  override def dispose(): Unit =
+    batch.dispose()
+    font.dispose()
+
+class MainMenuScreen(
+  val game: Main
+) extends Screen:
+
+  override def render(delta: Float): Unit =
+    ScreenUtils.clear(Color.BLACK)
+
+    game.viewport.apply()
+    game.batch.setProjectionMatrix(game.viewport.getCamera().combined)
+
+    game.batch.begin()
+    game.font.draw(game.batch, "Welcome to Drop!! ", 1, 1.5F)
+    game.font.draw(game.batch, "Tap anywhere to begin!", 1, 1)
+    game.batch.end()
+
+    if Gdx.input.isTouched() then
+      game.setScreen(GameScreen(game))
+      dispose()
+
+  override def resize(width: Int, height: Int): Unit =
+    game.viewport.update(width, height, true)
+
+  override def dispose(): Unit = {}
+  override def hide(): Unit = {}
+  override def pause(): Unit = {}
+  override def show(): Unit = {}
+  override def resume(): Unit = {}
+
+class GameScreen(
+  game: Main
+) extends Screen:
+
+  val backgroundTexture= Texture("background.png")
+  val dropTexture = Texture("drop.png")
+
+  val bucketTexture = Texture("bucket.png")
+  val bucketSprite = Sprite(bucketTexture)
+  bucketSprite.setSize(1, 1)
+
+  val spriteBatch = SpriteBatch()
+  val viewport = FitViewport(8, 5)
+
+  val touchPos = new Vector2()
+
+  val dropSprites = Array[Sprite]()
+
+  val bucketRectangle = Rectangle()
+  val dropRectangle = Rectangle()
+
+  val dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"))
+  val music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"))
+  music.setLooping(true)
+  music.setVolume(0.5F)
 
   var dropTimer: Float = 0F
 
-  var bucketRectangle: Rectangle = null
-  var dropRectangle: Rectangle = null
-
-  override def create(): Unit =
-    backgroundTexture = Texture("background.png")
-    dropTexture = Texture("drop.png")
-
-    dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"))
-    music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"))
-
-    spriteBatch = SpriteBatch()
-    viewport = FitViewport(8, 5)
-
-    bucketSprite = Sprite(Texture("bucket.png"))
-    bucketSprite.setSize(1, 1)
-
-    touchPos = new Vector2()
-
-    dropSprites = Array()
-
-    bucketRectangle = Rectangle()
-    dropRectangle = Rectangle()
-
-    music.setLooping(true)
-    music.setVolume(0.5F)
+  override def show(): Unit =
     music.play()
 
-  override def resize(width: Int, height: Int): Unit =
-    viewport.update(width, height, true)
-
-  override def render(): Unit =
-    input()
-    logic()
+  override def render(delta: Float): Unit =
+    input(delta)
+    logic(delta)
     draw()
 
-  private def input(): Unit =
-    val delta = Gdx.graphics.getDeltaTime()
+  private def input(delta: Float): Unit =
     val speed = 4F
 
     if Gdx.input.isKeyPressed(Input.Keys.RIGHT) then
@@ -84,7 +120,7 @@ class Main extends ApplicationListener:
       viewport.unproject(touchPos)
       bucketSprite.setCenterX(touchPos.x)
 
-  private def logic(): Unit =
+  private def logic(delta: Float): Unit =
     val worldWidth = viewport.getWorldWidth()
     val bucketWidth = bucketSprite.getWidth()
     val bucketHeight = bucketSprite.getHeight()
@@ -140,6 +176,16 @@ class Main extends ApplicationListener:
     sprite.setY(worldHeight)
     dropSprites.add(sprite)
 
+  override def resize(width: Int, height: Int): Unit =
+    viewport.update(width, height, true)
+
+  override def dispose(): Unit =
+    backgroundTexture.dispose()
+    dropSound.dispose()
+    music.dispose()
+    dropTexture.dispose()
+    bucketTexture.dispose()
+
+  override def hide(): Unit = {}
   override def pause(): Unit = {}
   override def resume(): Unit = {}
-  override def dispose(): Unit = {}
